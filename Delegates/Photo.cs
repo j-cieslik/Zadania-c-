d79ce0c0ca2da771e14874c99d2f32fb69f7e1c7
Kuba <jciesmail@gmail.com>
2020-12-06 20:20:16 +0100

    Add Delegates

---
+++ Delegates/Photo.cs
@@ -0,0 +1,14 @@
+namespace Delegates
+{
+  public class Photo
+    {
+        public static Photo Load(string path)
+        {
+            return new Photo();
+        }
+        public void Save()
+        {
+            
+        }
+    }
+}
---
+++ Delegates/PhotoFilters.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Delegates
+{
+  public class PhotoFilters
+    {
+        public void ApplyBrightness(Photo photo)
+        {
+            Console.WriteLine("Apply brightness");
+        }
+        public void ApplyContrast(Photo photo)
+        {
+            Console.WriteLine("Apply Contrast");
+        }
+        public void Resize(Photo photo)
+        {
+            Console.WriteLine("Resize photo");
+        }
+    }   
+}
