e8a4d6bc5dbe703de72fb8f59abea5a55c457d62
Kuba <jciesmail@gmail.com>
2020-12-06 23:24:19 +0100

    Events

---
+++ Events/VideoEncoder.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Threading;
+
+namespace Events
+{
+    public class VideoEventArgs : EventArgs
+    {
+        public Video Video { get; set; }
+    }
+
+  public class VideoEncoder
+  {
+
+    // 1 - Define a delegate
+    // 2 - Define an event based on that delegate
+    // 3 - Raise the event
+
+
+    // public delegate void VideoEncodedEventHandler(object source, VideoEventArgs args);
+
+    // EventHandler
+    // EventHandler<TEventArgs>
+
+    // public event VideoEncodedEventHandler VideoEncoded;
+
+    public event EventHandler<VideoEventArgs> VideoEncoded;
+
+    public void Encode(Video video)
+    {
+      Console.WriteLine("Encoding Video...");
+      Thread.Sleep(3000);
+
+      OnVideoEncoded(video);
+    }
+
+    protected virtual void OnVideoEncoded(Video video)
+    {
+      if (VideoEncoded != null)
+        VideoEncoded(this, new VideoEventArgs(){ Video = video });
+    }
+  }
+}
