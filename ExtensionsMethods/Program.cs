8ca9d1d596b5c1cf3cdd089f9cb039413914b136
Kuba <jciesmail@gmail.com>
2020-12-07 21:23:51 +0100

    Add Extensions Methods

---
+++ ExtensionsMethods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ExtensionsMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string post = "This is supposed to be a long blog blah blah blah blah balah ...";
+
+            var shortenedPost = post.Shorten(5);
+
+            Console.WriteLine(shortenedPost);
+        }
+    }
+
+}
